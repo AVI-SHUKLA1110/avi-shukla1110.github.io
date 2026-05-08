e26aa167d79fe278ddb9e456abe1fe9c6031d376
thetechnofeak <ashikla693@gmail.com>
2026-05-08 11:57:43 +0530

    Updates

---
+++ assets/cybersecurity-roadmap-b6DJXWX-.js
@@ -0,0 +1,81 @@
+const e=`# Start Cybersecurity the Minimalist Way — The Only Roadmap You Need 🛣️\r
+\r
+Cybersecurity feels overwhelming when you're starting out. Too many tools. Too many paths. Here's a simple, structured roadmap for beginners — focused only on what actually matters, based on my own experience and conversations with other professionals in the industry 👇\r
+\r
+## Let's Start\r
+\r
+![Perry the Platypus](https://media1.tenor.com/m/0Vi85rk0ISoAAAAd/perry-the-platypus-perry.gif)\r
+\r
+### Step 1: Infosec Basics\r
+(If you are new to computer science, I recommend watching this or else directly jump to later parts)\r
+\r
+- CompTIA A+ Certification Course\r
+- TryHackMe: Beginner Level - [Link](https://tryhackme.com/hacktivities)\r
+- Security Basics - CompTIA Security+ Certification Course\r
+\r
+### Step 2: Networking\r
+If you don't understand networking, cybersec will feel like magic.\r
+\r
+Learn:\r
+- How the internet works\r
+- TCP/IP, DNS, HTTP\r
+- IPv4 / IPv6\r
+- Routers, firewalls, Wi-Fi\r
+\r
+👉 CompTIA Network+ level knowledge is enough to start.\r
+➕ You can also practice on Cisco Packet Tracer software.\r
+\r
+### Step 3: Learn Linux\r
+This is where you'll be spending most of your time.\r
+\r
+Resources:\r
+- Linux Essentials for Hackers (YT)\r
+- [explainshell.com](http://explainshell.com) (bookmark this) - this I found out later but was really helpful\r
+- "OverTheWire" - Bandit challenges\r
+\r
+### Step 4: Programming (Scripting & Automation)\r
+Either Bash or Python.\r
+\r
+**Python:**\r
+- PhD Security - he is an amazing content creator in cybersec and you will learn a lot from his journey: [YouTube](https://youtube.com/watch?v=XWuP5Yf5ILI)\r
+- FreeCodeCamp - Offers tutorials for beginners and this one covers the basics of Python programming: [YouTube](https://youtube.com/watch?v=rfscVS0vtbw)\r
+- Paid but awesome courses - Python101 & Python201 (TCM Security)\r
+\r
+Well, if you are looking to go into web security, learning JavaScript would prove to be helpful:\r
+- Academind - an amazing playlist that I personally liked\r
+\r
+**For Bash:**\r
+- TryHackMe lesson is a good starter\r
+- Another great resource is this playlist by @HackerSploit\r
+\r
+### Step 5: Core Cybersecurity\r
+Now here comes the interesting part - you all have been waiting for 'HACKING'\r
+\r
+For intros, these two are my personal favorites:\r
+- Practical Ethical Hacking by @TCMSecurity\r
+- This course by FreeCodeCamp\r
+\r
+This will utilize the skills you learnt up till now such as Linux, Python, etc.\r
+\r
+🚩 After this point you are ready to start practicing on platforms such as @hackthebox_eu, @tryhackme, @picoctf.\r
+\r
+HTB is not so beginner-friendly so you might want to start from TryHackMe -> then move to Hack The Box and PicoCTF and other platforms for hands-on learning.\r
+\r
+### Step 6: Choose Your Niche\r
+Cybersecurity in itself is a huge domain so it is practically not feasible to learn everything when you start, and from the knowledge you collected from the above steps you have transitioned beyond a novice and thus it is time for you to explore diverse cybersecurity domains:\r
+\r
+For beginners you might want to learn about either of them - application security, network security, penetration testing, SOC, and other blue teaming stuff in general. (Don't forget most of the jobs are in blue teaming)\r
+\r
+And for experienced individuals it might be - binary exploitation, malware analysis, cryptography, etc. Identify your interests and strengths to choose a specific niche.\r
+\r
+After this point, each one has its own specific resources and roadmaps. Stay curious, dedicated, and keep exploring the dynamic world of cybersecurity.\r
+\r
+## Few Amazing People Whom You Should Follow in This Domain\r
+- [@thecybermentor](https://twitter.com/thecybermentor)\r
+- [@LiveOverflow](https://twitter.com/LiveOverflow)\r
+- [@_JohnHammond](https://twitter.com/_JohnHammond)\r
+- [@NahamSec](https://twitter.com/NahamSec)\r
+- [@NetworkChuck](https://twitter.com/NetworkChuck)\r
+- [@davidbombal](https://twitter.com/davidbombal)\r
+\r
+Do follow me, if you want to see more amazing contents on Cybersec, web3 and tech in general.`;export{e as default};
